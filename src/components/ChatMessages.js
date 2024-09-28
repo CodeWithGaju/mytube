d5910370f6757a6be019cbe2e88e72e028c24290
Gajanand Khatriya <164136299+CodeWithGaju@users.noreply.github.com>
2024-09-28 18:22:29 +0530

    changes in css

--- src/components/ChatMessages.js
+++ src/components/ChatMessages.js
@@ -7,9 +7,9 @@ const ChatMessages = ({userName,userComments}) => {
   return (
     <div>
         <div className='flex m-1 shadow-md dark:bg-black'>
-            <img className="w-5 ml-2 h-12 py-2 " src={UserIcon}></img>
-            <h1 className='w-20 text-lg  font-bold pt-2  ml-3'>{userName}</h1>
-            <p className='w-80 px-2  h-14 pt-2 '>{userComments}</p>
+            <img className="w-3 md:w-5 ml-2 h-12 py-2 " src={UserIcon}></img>
+            <h1 className='w-20 text-sm md:text-lg  font-bold pt-3 md:pt-2  ml-3'>{userName}</h1>
+            <p className='w-80 px-2 text-xs md:text-base  md:h-14 pt-2 '>{userComments}</p>
         </div>
     </div>
   )
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -14,7 +14,7 @@ const WatchPage = () => {
   const toogleSideBar = useSelector((store)=>store.app.isMenuOpen);
   const style = toogleSideBar ? "w-[300px] md:w-[1300px] md:flex " : "w-[380px] md:w-[1535px] md:flex ";
   const frameStyle = toogleSideBar ? "w-[320px] h-[150px]  md:w-[900px] md:h-[500px] mt-2 " : "w-[350px] md:w-[1050px] md:h-[500px] md:ml-14 mt-2 mx-auto";
-  const chatStyle = toogleSideBar ? "w-[380px] md:w-[400px] md:ml-4  " : "w-[380px] md:w-[450px] md:ml-4 mt-2 ";
+  const chatStyle = toogleSideBar ? "w-[280px] md:w-[400px] md:ml-4  " : "w-[380px] md:w-[450px] md:ml-4 mt-2 ";
 
   useEffect(()=>{
      dispatch(toogleMenu())
@@ -43,8 +43,8 @@ const WatchPage = () => {
       <div className={chatStyle}>
            <LiveChat/>
            <form  onSubmit={(e)=>e.preventDefault()} className='h-12 px-2 py-2 w-full border border-gray-200'>
-               <input type="text" placeholder='Chat' className='h-8 px-2 ml-10 py-1 w-2/3 shadow-lg rounded-l-full bg-gray-100 dark:bg-opacity-20' onChange={(e)=>setChatInput(e.target.value)} value={ChatInput} />
-               <button className=' px-3  shadow-xl py-1 ml-0.5  h-8 rounded-r-lg bg-gray-300 dark:bg-opacity-60 ' onClick={handleChatSubmit}>Send</button>
+               <input type="text" placeholder='Chat' className='h-6 md:h-8 px-2 ml-5 md:ml-10 md:text-base text-sm py-1 w-2/3 shadow-lg rounded-l-full bg-gray-100 dark:bg-opacity-20' onChange={(e)=>setChatInput(e.target.value)} value={ChatInput} />
+               <button className=' px-3  shadow-xl md:py-1 ml-0.5  h-6 md:h-8 rounded-r-lg bg-gray-300 dark:bg-opacity-60 text-sm justify-center md:text-base' onClick={handleChatSubmit}>Send</button>
 
             </form>
       </div>
